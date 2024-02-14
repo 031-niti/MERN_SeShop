655511a9f5c30ceaaca1458522b296519c949975
031-niti <earthza6263@gmail.com>
2024-02-14 16:41:27 +0700

    final

--- client/routes/Router.jsx
+++ client/routes/Router.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 import { createBrowserRouter } from "react-router-dom";
 import Main from '../src/layout/Main';
 import Home from '../src/pages/home/Home';
+import ProductList from '../src/pages/shop/ProductList';
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Main/>,
+        element: <Main />,
         children:[
             {
                 path: "/",
                 element: <Home/>,
+            },
+            {
+                path: "/shop",
+                element: <ProductList />,
             }
-        ]
+        ],
     },
 ]);
 
