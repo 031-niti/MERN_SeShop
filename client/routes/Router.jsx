be44a583e4dc52404d939fa978247fe23780ad34
031-niti <earthza6263@gmail.com>
2024-02-15 16:33:55 +0700

    setting login signup with firebase

--- client/routes/Router.jsx
+++ client/routes/Router.jsx
@@ -3,14 +3,16 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from '../src/layout/Main';
 import Home from '../src/pages/home/Home';
 import ProductList from '../src/pages/shop/ProductList';
+import SignUp from '../src/components/SignUp';
+import SignIn from '../src/components/SignIn';
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Main />,
-        children:[
+        children: [
             {
                 path: "/",
-                element: <Home/>,
+                element: <Home />,
             },
             {
                 path: "/shop",
@@ -18,6 +20,14 @@ const router = createBrowserRouter([
             }
         ],
     },
+    {
+        path: "/signup",
+        element: <SignUp />
+    }, 
+    {
+        path: "/signin",
+        element: <SignIn/>
+    }
 ]);
 
 export default router;
